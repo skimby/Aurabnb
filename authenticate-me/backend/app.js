e4142f166a38cd724db7959dd182affd41d74f4c
Sally <sallykim13@gmail.com>
2022-05-14 15:27:25 -0500

    commit

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//initialize express application
+const app = express();
+
+//connect morgan middlewarre- logging info about req and res
+app.use(morgan('dev'));
+
+//cookie parser middlewar
+app.use(cookieParser());
+//parsing json body requests with content type of app/json
+app.use(express.json());
+
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
