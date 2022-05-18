a59c0eaf9c1bd2402baff7e62a1b7eba93df09e4
Sally <sallykim13@gmail.com>
2022-05-18 17:40:25 -0500

    added images

---
+++ backend/db/seeders/20220518223852-add-images-seed-data.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Images', [
+      {
+        imageableType: 'spot',
+        url: ''
+      }
+    ])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
