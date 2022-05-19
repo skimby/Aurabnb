236910c283abf16576d91edcbaeae07bae374b17
Sally <sallykim13@gmail.com>
2022-05-18 23:49:05 -0500

    get current user endpoint done!

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,41 +11,6 @@ router.post('/test', function (req, res) {
 });
 
 
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-
-
-router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            email: 'ladygaga@gmail.com'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-});
-
-
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
 
 
 module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -45,5 +45,46 @@ router.post(
     }
 );
 
+// GET CURRENT USER
+// GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        // attributes: { exclude: ['isHost', 'createdAt', 'updatedAt'] },
+        where: {
+            email: 'ladygaga@gmail.com'
+        },
+
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+});
+
+// GET CURRENT USER
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+router.get('/restore-user', restoreUser, (req, res) => {
+    return res.json(req.user);
+}
+);
+
+// GET CURRENT USER
+// GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+router.get('/me', requireAuth, (req, res) => {
+    const user = req.user
+
+    res.status(200);
+    return res.json({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email
+    });
+}
+);
+
 
 module.exports = router;
