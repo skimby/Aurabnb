aff0c4fede7c82162c9c8c334236288d2e856635
Sally <sallykim13@gmail.com>
2022-05-23 23:57:08 -0500

    create spot endpoint done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -15,6 +15,40 @@ const { Op } = require("sequelize");
 
 router.use('/session', sessionRouter);
 
+// SPOT VALIDATION ERROR
+//checks the body of new spot post request
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        // .notEmpty()
+        .withMessage("Street address is required"),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage("City is required"),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage("State is required"),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage("Country is required"),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .withMessage("Latitude is not valid"),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .withMessage("Longitude is not valid"),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage("Name must be less than 50 characters"),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+];
+
 
 
 //ROUTES
@@ -62,7 +96,7 @@ router.get('/', async (req, res) => {
     return res.json({ Spots });
 })
 
-router.post('/', restoreUser, async (req, res) => {
+router.post('/', restoreUser, validateSpot, async (req, res) => {
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
     const spotCount = await Spot.count();
