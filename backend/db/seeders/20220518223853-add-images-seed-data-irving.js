0591afa4247ef3eeb38955aa83ec1c32c9acb9c1
Sally <sallykim13@gmail.com>
2022-07-18 02:32:47 -0500

    g

--- backend/db/seeders/20220518223853-add-images-seed-data-irving.js
+++ backend/db/seeders/20220518223853-add-images-seed-data-irving.js
@@ -11,13 +11,13 @@ module.exports = {
       },
       {
         imageableType: 'Spot',
-        url: 'https://airbnb-images-bucket.s3.us-east-2.amazonaws.com/irving/4ed0f6f7-59d5-4f9a-9e76-93fa98c67456.jpg',
+        url: 'https://airbnb-images-bucket.s3.us-east-2.amazonaws.com/irving/27164898-86e0-453e-88f3-0e34ea1d6ea0.jpg',
         spotId: 6,
         reviewId: null
       },
       {
         imageableType: 'Spot',
-        url: 'https://airbnb-images-bucket.s3.us-east-2.amazonaws.com/irving/79712d85-c95e-4419-8237-2f23dba6857d.jpg',
+        url: 'https://airbnb-images-bucket.s3.us-east-2.amazonaws.com/irving/ad95ffca-a49a-41e9-aed6-981c48f47a5c.jpg',
         spotId: 6,
         reviewId: null
       },
